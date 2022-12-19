9152f8551708cd0fb4ff20b20bf99dea08b13706
ghaneeyat09 <69092604+ghaneeyat09@users.noreply.github.com>
2022-12-19 13:44:46 +0100

    mmm

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,6 +6,14 @@ const Portfolio = () => {
             <h1 className="header">Portfolio</h1>
             <div className="collection">
                 <div className="wrapper height">
+                   <img src={process.env.PUBLIC_URL + "/images/img1.jpg"} alt="" className="img"/>
+                   <div className="blind">
+                       <a href="https://alyusrohmodestwears.com/" className="link"><h1 className="alyusroh">Alyusroh modest wears</h1></a>
+                       <p>Fashion Brand</p>
+                       <p className="tech">MERN stack</p>
+                   </div>
+                </div>
+                <div className="wrapper height">
                    <img src={process.env.PUBLIC_URL + "/images/img4.jpg"} alt="" className="img"/>
                    <div className="blind">
                        <a href="https://ghaneeyat09.github.io/goVacation/" className="link"><h1>Govac</h1></a>
@@ -29,14 +37,14 @@ const Portfolio = () => {
                        <p className="tech">Vue app</p>
                    </div>
                 </div>
-                <div className="wrapper height">
+                {/* <div className="wrapper height">
                    <img src={process.env.PUBLIC_URL + "/images/img3.jpeg"}alt="" className="img"/>
                    <div className="blind">
                        <a href="https://ghaneeyat09.github.io/fudjungle/" className="link"><h1>Taskify</h1></a>
                        <p>Todo/Task Management</p>
                        <p className="tech">Vue and Node app</p>
                    </div>
-                </div>
+                </div> */}
                 <div className="wrapper height">
                    <img src={process.env.PUBLIC_URL + "/images/perfume.jpg"}alt="" className="img"/>
                    <div className="blind">
@@ -45,14 +53,14 @@ const Portfolio = () => {
                        <p className="tech">HTML & CSS</p>
                    </div>
                 </div>
-                <div className="wrapper height">
+                {/* <div className="wrapper height">
                    <img src={process.env.PUBLIC_URL + "/images/blog.jpg"}alt="" className="img"/>
                    <div className="blind">
                        <a href="https://ghaneeyat09.github.io/fudjungle/" className="link"><h1>Afriblog</h1></a>
                        <p>Blogging App</p>
                        <p className="tech">React, Node and MySQL App</p>
                    </div>
-                </div>
+                </div> */}
                 
             </div>
         </div>
